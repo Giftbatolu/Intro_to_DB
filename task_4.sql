61ea71ddfe422c4f9f2283790dd99c6472df8436
giftbatolu <sekinatoyelami@gmail.com>
2025-10-19 18:10:34 +0100

    file: Add task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,4 @@
--- This script prints the full description of the 'books' table
--- without using DESCRIBE or EXPLAIN
+-- This script prints the full description of the 'books' table without using DESCRIBE or EXPLAIN
 
 USE alx_book_store;
 SHOW CREATE TABLE books;
